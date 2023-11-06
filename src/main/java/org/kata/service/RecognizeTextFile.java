2240d4e256b30160731204b428264356709b499d
NikK <38474033+Xy1aR@users.noreply.github.com>
2023-11-06 11:01:24 +0300

    done document recognition task, added standard docs pics and tessdata (#2)

--- src/main/java/org/kata/service/RecognizeTextFile.java
+++ src/main/java/org/kata/service/RecognizeTextFile.java
@@ -4,45 +4,51 @@ package org.kata.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
 
 @Service
 @Slf4j
 public class RecognizeTextFile {
 
-    public String recognizeName(String recognizedText) {
-        //TODO реализовать распознование
-        return "name";
+    public String recognizeName(List<String> recognizedText) {
+        return recognizedText.get(1);
     }
 
-    public String recognizeSurname(String recognizedText) {
-        //TODO реализовать распознование
-        return "surname";
+    public String recognizeSurname(List<String> recognizedText) {
+        return recognizedText.get(0);
     }
 
-    public String recognizePatronymic(String recognizedText) {
-        //TODO реализовать распознование
-        return "patronymic";
+    public String recognizePatronymic(List<String> recognizedText) {
+        return recognizedText.get(2);
     }
 
-    public String recognizeNumber(String recognizedText) {
-        //TODO реализовать распознование
-        return "Number";
+    public String recognizeNumber(List<String> recognizedText) {
+        return recognizedText.get(2);
     }
 
-    public String recognizeSerial(String recognizedText) {
-        //TODO реализовать распознование
-        return "Serial";
+    public String recognizeSerial(List<String> recognizedText) {
+        return recognizedText.get(0) + recognizedText.get(1);
     }
 
-    public Date recognizeExpirationDate(String recognizedText) {
-        //TODO реализовать распознование
-        return new Date();
+    public Date recognizeExpirationDate(List<String> recognizedText) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            LocalDate date = LocalDate.parse(recognizedText.get(1), formatter);
+            return Date.from(date.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+        } catch (Exception e) {
+            return null;
+        }
     }
 
-    public Date recognizeIssueDate(String recognizedText) {
-        //TODO реализовать распознование
-        return new Date();
+    public Date recognizeIssueDate(List<String> recognizedText) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate date = LocalDate.parse(recognizedText.get(0), formatter);
+        return Date.from(date.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
     }
+
 
 }
--- src/main/java/org/kata/service/impl/DocumentServiceImpl.java
+++ src/main/java/org/kata/service/impl/DocumentServiceImpl.java
@@ -2,6 +2,7 @@ package org.kata.service.impl;
 
 import io.netty.util.internal.StringUtil;
 import lombok.extern.slf4j.Slf4j;
+import net.sourceforge.tess4j.ITessAPI;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 import org.kata.config.UrlProperties;
@@ -15,7 +16,14 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @Slf4j
@@ -23,6 +31,19 @@ public class DocumentServiceImpl implements DocumentService {
     private final UrlProperties urlProperties;
     private final WebClient updateWebClient;
     private final RecognizeTextFile recognizeTextFile;
+    private static final String TESSDATA_SRC = "src/main/resources/tessdata";
+    private static final String TRAINED_DATA = "rus-best";
+    private static final int PASSPORT_WIDTH = 900;
+    private static final int PASSPORT_HEIGHT = 1200;
+    private static final Rectangle PASSPORT_FULL_NAME_BOX = new Rectangle(370, 630, 405, 225);
+    private static final Rectangle PASSPORT_SERIAL_NUMS_BOX = new Rectangle(775, 125, 127, 451);
+    private static final Rectangle PASSPORT_ISSUE_DATA_BOX = new Rectangle(71, 236, 300, 60);
+    private static final int DRIVING_LICENSE_WIDTH = 1200;
+    private static final int DRIVING_LICENSE_HEIGHT = 800;
+    private static final Rectangle DRIVING_LICENSE_FULL_NAME_BOX = new Rectangle(480, 120, 680, 175);
+    private static final Rectangle DRIVING_LICENSE_SERIAL_NUMS_BOX = new Rectangle(480, 500, 250, 90);
+    private static final Rectangle DRIVING_LICENSE_ISSUE_DATA_BOX = new Rectangle(480, 410, 586, 60);
+
 
     public DocumentServiceImpl(UrlProperties urlProperties, RecognizeTextFile recognizeTextFile) {
         this.urlProperties = urlProperties;
@@ -31,28 +52,63 @@ public class DocumentServiceImpl implements DocumentService {
     }
 
     public RecognizeDocumentDto recognizeDocument(String icp, DocumentType type, MultipartFile file) {
+        List<String> documentInfo;
+        List<String> itemsFullName;
+        List<String> itemsSerialNums;
+        List<String> itemsIssueData = new ArrayList<>();
         Tesseract tesseract = new Tesseract();
-        //tesseract.setDatapath("/path_to_tessdata"); // Укажите путь к tessdata
+        tesseract.setDatapath(TESSDATA_SRC);
+        tesseract.setLanguage(TRAINED_DATA);
+        tesseract.setPageSegMode(ITessAPI.TessPageSegMode.PSM_AUTO_ONLY);
 
-        String recognizedText;
-        try {
-            recognizedText = tesseract.doOCR((BufferedImage) file);
-        } catch (TesseractException e) {
-            throw new DocumentsRecognitionException("Ошибка при распознавании документа");
+        switch (type) {
+            case RF_PASSPORT -> {
+                documentInfo = getDocumentText(tesseract, file, PASSPORT_WIDTH, PASSPORT_HEIGHT,
+                        PASSPORT_FULL_NAME_BOX, PASSPORT_SERIAL_NUMS_BOX, PASSPORT_ISSUE_DATA_BOX);
+                itemsFullName = new ArrayList<>(List.of(documentInfo.get(0).replaceAll("\\n", " ")
+                        .replaceAll("\\s+", " ")
+                        .split("\\s")));
+                itemsSerialNums = new ArrayList<>(List.of(documentInfo.get(1).replaceAll("[^0-9\\s]", "")
+                        .replaceAll("\\n", " ")
+                        .replaceAll("\\s+", " ")
+                        .split("\\s")));
+                Pattern pattern = Pattern.compile("\\d\\d.\\d\\d.\\d\\d\\d\\d");
+                Matcher matcher = pattern.matcher(documentInfo.get(2));
+                while (matcher.find()) {
+                    itemsIssueData.add(documentInfo.get(2).substring(matcher.start(), matcher.end()));
+                }
+            }
+            case RF_DRIVING_LICENSE -> {
+                documentInfo = getDocumentText(tesseract, file, DRIVING_LICENSE_WIDTH, DRIVING_LICENSE_HEIGHT,
+                        DRIVING_LICENSE_FULL_NAME_BOX, DRIVING_LICENSE_SERIAL_NUMS_BOX, DRIVING_LICENSE_ISSUE_DATA_BOX);
+                itemsFullName = new ArrayList<>(List.of(documentInfo.get(0).replaceAll("\\n", " ")
+                        .replaceAll("\\s\\s", " ")
+                        .split("\\s")));
+                itemsFullName.remove(1);
+                itemsSerialNums = new ArrayList<>(List.of(documentInfo.get(1).replaceAll("\\n", " ")
+                        .replaceAll("\\s\\s", " ")
+                        .split("\\s")));
+                itemsSerialNums.remove(0);
+                itemsSerialNums.remove(0);
+                Pattern pattern = Pattern.compile("\\d\\d.\\d\\d.\\d\\d\\d\\d");
+                Matcher matcher = pattern.matcher(documentInfo.get(2));
+                while (matcher.find()) {
+                    itemsIssueData.add(documentInfo.get(2).substring(matcher.start(), matcher.end()));
+                }
+            }
+            default -> throw new DocumentsRecognitionException("Неверный тип документа");
         }
-        if (StringUtil.isNullOrEmpty(recognizedText)) {
-            throw new DocumentsRecognitionException("Не удалось распознать документ");
-        }
+
         RecognizeDocumentDto recognizeDocumentDto = RecognizeDocumentDto.builder()
                 .icp(icp)
-                .name(recognizeTextFile.recognizeName(recognizedText))
-                .surname(recognizeTextFile.recognizeSurname(recognizedText))
-                .patronymic(recognizeTextFile.recognizePatronymic(recognizedText))
+                .name(recognizeTextFile.recognizeName(itemsFullName))
+                .surname(recognizeTextFile.recognizeSurname(itemsFullName))
+                .patronymic(recognizeTextFile.recognizePatronymic(itemsFullName))
                 .documentType(type)
-                .documentNumber(recognizeTextFile.recognizeNumber(recognizedText))
-                .documentSerial(recognizeTextFile.recognizeSerial(recognizedText))
-                .expirationDate(recognizeTextFile.recognizeExpirationDate(recognizedText))
-                .issueDate(recognizeTextFile.recognizeIssueDate(recognizedText))
+                .documentNumber(recognizeTextFile.recognizeNumber(itemsSerialNums))
+                .documentSerial(recognizeTextFile.recognizeSerial(itemsSerialNums))
+                .expirationDate(recognizeTextFile.recognizeExpirationDate(itemsIssueData))
+                .issueDate(recognizeTextFile.recognizeIssueDate(itemsIssueData))
                 .build();
 
         postDocument(recognizeDocumentDto);
@@ -72,5 +128,41 @@ public class DocumentServiceImpl implements DocumentService {
                 .block();
 
         log.info("Распознанный документ успешно отправлен на обновление");
+    }
+
+    private BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
+        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics2D = resizedImage.createGraphics();
+        graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
+        graphics2D.dispose();
+        return resizedImage;
+    }
+
+    private List<String> getDocumentText (Tesseract tesseract, MultipartFile file, int targetWidth,
+                                          int targetHeight, Rectangle fullNameBox, Rectangle serialNumsBox,
+                                          Rectangle issueDateBox) {
+        BufferedImage image;
+        String recognizedTextFullName;
+        String recognizedTextSerialNums;
+        String recognizedTextIssueDate;
+        List<String> result = new ArrayList<>();
+
+        try {
+            image = resizeImage(ImageIO.read(file.getInputStream()), targetWidth, targetHeight);
+            recognizedTextFullName = tesseract.doOCR(image, fullNameBox);
+            recognizedTextSerialNums = tesseract.doOCR(image, serialNumsBox);
+            recognizedTextIssueDate = tesseract.doOCR(image, issueDateBox);
+        } catch (IOException | TesseractException e) {
+            throw new DocumentsRecognitionException("Ошибка при чтении документа");
+        }
+        if (StringUtil.isNullOrEmpty(recognizedTextFullName) || StringUtil.isNullOrEmpty(recognizedTextSerialNums)) {
+            throw new DocumentsRecognitionException("Не удалось распознать документ");
+        }
+
+        result.add(recognizedTextFullName);
+        result.add(recognizedTextSerialNums);
+        result.add(recognizedTextIssueDate);
+
+        return result;
     }
 }
